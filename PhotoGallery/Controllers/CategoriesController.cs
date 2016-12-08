eaa26bfc37d61d835cc3b7546749b5f8e79ee801
TeodorStoyanov <tedy17@abv.bg>
2016-12-08 22:04:28 +0200

    Category Model uploaded

---
+++ PhotoGallery/Controllers/CategoriesController.cs
@@ -0,0 +1,24 @@
+﻿using PhotoGallery.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PhotoGallery.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        // GET: Category
+        public ActionResult Index()
+        {
+            using (var database = new ApplicationDbContext())
+            {
+                var categories = database.Categories
+                    .ToList();
+
+                return View(categories);
+            }
+        }
+    }
+}
