4fb13afcadabcbe1489b761272eb306fa5d76f4f
TeodorStoyanov <tedy17@abv.bg>
2016-12-08 11:43:17 +0200

    Changed the migrations so you can change the data base without deleting it.

--- PhotoGallery/Startup.cs
+++ PhotoGallery/Startup.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.Owin;
 using Owin;
+using PhotoGallery.Models;
+using PhotoGallery.Migrations;
+using System.Data.Entity;
 
 [assembly: OwinStartupAttribute(typeof(PhotoGallery.Startup))]
 namespace PhotoGallery
@@ -8,6 +11,8 @@ namespace PhotoGallery
     {
         public void Configuration(IAppBuilder app)
         {
+            Database.SetInitializer(
+                new MigrateDatabaseToLatestVersion<ApplicationDbContext, Configuration>());
             ConfigureAuth(app);
         }
     }
