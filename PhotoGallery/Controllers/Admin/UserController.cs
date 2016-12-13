79eba72d472f29bce1bea103c54be699fcc08edc
TeodorStoyanov <tedy17@abv.bg>
2016-12-14 01:47:48 +0200

    ADMIN MENU: Listing all users and editing/saving changes working. Delete button not working.

--- PhotoGallery/Controllers/Admin/UserController.cs
+++ PhotoGallery/Controllers/Admin/UserController.cs
@@ -1,7 +1,12 @@
-﻿using PhotoGallery.Models;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using PhotoGallery.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -19,7 +24,137 @@ namespace PhotoGallery.Controllers.Admin
                 var users = db.Users
                     .ToList();
 
+                var admins = GetAdminUserNames(users, db);
+                ViewBag.Admins = admins;
+
                 return View(users);
+            }
+        }
+
+        private HashSet<string> GetAdminUserNames(List<ApplicationUser> users, ApplicationDbContext context)
+        {
+            var userManager = new UserManager<ApplicationUser>(
+                new UserStore<ApplicationUser>(context));
+
+            var admins = new HashSet<string>();
+
+            foreach(var user in users)
+            {
+                if(userManager.IsInRole(user.Id,"Admin"))
+                {
+                    admins.Add(user.UserName);
+                }
+            }
+
+            return admins;
+        }
+
+        //GET: User/Edit
+        public ActionResult Edit(string id)
+        {
+            if(id==null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            using (db)
+            {
+                var user = db.Users
+                    .Where(u => u.Id == id)
+                    .First();
+
+                if(user==null)
+                {
+                    return HttpNotFound();
+                }
+
+                var viewModel = new EditUserViewModel();
+                viewModel.User = user;
+                viewModel.Roles = GetUserRoles(user, db);
+
+                return View(viewModel);
+            }
+        }
+
+        private List<Role> GetUserRoles(ApplicationUser user, ApplicationDbContext db)
+        {
+            var userManager = Request
+                .GetOwinContext()
+                .GetUserManager<ApplicationUserManager>();
+
+            var roles = db.Roles
+                .Select(r => r.Name)
+                .OrderBy(r => r)
+                .ToList();
+
+            var userRoles = new List<Role>();
+
+            foreach(var roleName in roles)
+            {
+                var role = new Role { Name = roleName };
+
+                if(userManager.IsInRole(user.Id,roleName))
+                {
+                    role.IsSelected = true;
+                }
+
+                userRoles.Add(role);
+            }
+
+            return userRoles;
+        }
+
+        //POST: User/Edit
+        [HttpPost]
+        public ActionResult Edit(string id,EditUserViewModel viewModel)
+        {
+            if(ModelState.IsValid)
+            {
+                using (db)
+                {
+                    var user = db.Users.FirstOrDefault(u => u.Id == id);
+
+                    if(user==null)
+                    {
+                        return HttpNotFound();
+                    }
+
+                    if(!string.IsNullOrEmpty(viewModel.Password))
+                    {
+                        var hasher = new PasswordHasher();
+                        var passwordHash = hasher.HashPassword(viewModel.Password);
+                        user.PasswordHash = passwordHash;
+                    }
+
+                    user.Email = viewModel.User.Email;
+                    user.FullName = viewModel.User.FullName;
+                    user.UserName = viewModel.User.Email;
+                    this.SetUserRoles(viewModel, user, db);
+
+                    db.Entry(user).State = EntityState.Modified;
+                    db.SaveChanges();
+
+                    return RedirectToAction("List");
+                }
+            }
+            return View(viewModel);
+        }
+
+        private void SetUserRoles(EditUserViewModel viewModel, ApplicationUser user, ApplicationDbContext context)
+        {
+            var userManager = HttpContext.GetOwinContext()
+                .GetUserManager<ApplicationUserManager>();
+
+            foreach (var role in viewModel.Roles)
+            {
+                if(role.IsSelected && !userManager.IsInRole(user.Id,role.Name))
+                {
+                    userManager.AddToRole(user.Id, role.Name);
+                }
+                else if(!role.IsSelected && userManager.IsInRole(user.Id,role.Name))
+                {
+                    userManager.RemoveFromRole(user.Id, role.Name);
+                }
             }
         }
     }
