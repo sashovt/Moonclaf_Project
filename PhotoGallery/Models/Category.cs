12eeb4247c0b0eca0ea56b365973f9eb7de06998
CrossAce <acexdark@gmail.com>
2016-12-18 00:47:58 +0200

    Again Update

---
+++ PhotoGallery/Models/Category.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace PhotoGallery.Models
+{
+    public class Category
+    {
+        private ICollection<Photo> photos;
+        public Category()
+        {
+            this.photos = new HashSet<Photo>();
+        }
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [Index(IsUnique = true)]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        public virtual ICollection<Photo> Photos
+        {
+            get { return photos; }
+            set { photos = value; }
+        }
+    }
+}
---
+++ PhotoGallery/Models/UploadPhotoViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace PhotoGallery.Models
+{
+    public class UploadPhotoViewModel
+    {
+        public UploadPhotoViewModel()
+        {
+            this.DateAdded = DateTime.Now;
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Title { get; set; }
+
+        [Required]
+        public byte[] Image { get; set; }
+
+        [Required]
+        public DateTime DateAdded { get; set; }
+
+        [ForeignKey("Author")]
+        public string AuthorId { get; set; }
+
+        public virtual ApplicationUser Author { get; set; }
+
+        public IEnumerable<SelectList> Categories { get; set; }
+
+        public bool IsAuthor(string name)
+        {
+            return this.Author.UserName.Equals(name);
+        }
+    }
+}
