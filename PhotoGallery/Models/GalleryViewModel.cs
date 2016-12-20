5ecf92203dc908284003eb412538e5ded51dc9bb
CrossAce <acexdark@gmail.com>
2016-12-21 00:15:48 +0200

    Final Update I hope

---
+++ PhotoGallery/Models/GalleryViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PhotoGallery.Models
+{
+    public class GalleryViewModel
+    {
+        public IEnumerable<Photo> photo { get; set; }
+        public ICollection<Category> category { get; set; }
+
+        public GalleryViewModel(IEnumerable<Photo> photo, ICollection<Category> category)
+        {
+            this.photo = photo;
+            this.category = category;
+        }
+    }
+}
